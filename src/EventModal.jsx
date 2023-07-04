b4b4094a05e56aeeb46eff6d401eb68be7b1228b
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-04 15:15:38 +0300

    feat: update eventModal

--- src/EventModal.jsx
+++ src/EventModal.jsx
@@ -6,7 +6,9 @@ import ScheduleIcon from '@mui/icons-material/Schedule';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
 import DescriptionIcon from '@mui/icons-material/Description';
+import EditIcon from '@mui/icons-material/Edit';
 import "./EventModal.css";
+import dayjs from 'dayjs';
 
 const labels = ['red', 'green', 'blue',  'purple', 'pink', 'orange'];
 
@@ -43,9 +45,11 @@ const EventModal = () => {
     };
     setShowEventModal(false);
   };
+  const [isFormEdit, setIsFormEdit] = useState(false);
+
   return (
     <div className='event-modal'>
-      <form className='event-modal__form'>
+      <form className='event-modal__form' onSubmit={handleSubmit}>
         <button onClick={() => setShowEventModal(false)} className='event-modal__close'>
           <CloseIcon/>
         </button>
@@ -61,47 +65,78 @@ const EventModal = () => {
         >
           <DeleteIcon/>
         </button>}
-        <div className='event-modal__element'>
-          <input 
-            type='text'
-            name='title'
-            placeholder='Add title'
-            className='event-modal__input'
-            required
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            />
-        </div>
-        <div className='event-modal__element'>
-          <ScheduleIcon/>
-          <p>{daySelected.format('dddd, MMMM DD')}</p>
-        </div>
-        <div className='event-modal__element'>
-          <DescriptionIcon/>
-          <input 
-            type='text'
-            name='description'
-            placeholder='Add description'
-            className='event-modal__input'
-            required
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-          />
-        </div>
-        <div className='event-modal__element'> 
-          {labels.map((label, i) => (
-            <span
-              key={i}
-              onClick={() => setSelectedLabel(label)}
-              className='event-modal__label' 
-              style={{backgroundColor: label}}>
-                {selectedLabel === label && <CheckIcon/>}
-              </span>
-          ))}
-        </div>
-        <button onClick={handleSubmit} type='submit' className='event-modal__save'>
-          {selectedEvent ? 'Update' : 'Save'}
+        {selectedEvent && <button
+          onClick={(e) => {
+            e.preventDefault();
+            setIsFormEdit(!isFormEdit)
+          }}
+          className='event-modal__edit'
+        >
+          <EditIcon />
         </button>
+        }
+        {(isFormEdit || !selectedEvent) &&
+          <div className='event-modal__elements'>
+          <div className='event-modal__element'>
+            <input 
+              type='text'
+              name='title'
+              placeholder='Add title'
+              className='event-modal__input'
+              required
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              />
+          </div>
+          <div className='event-modal__element'>
+            <ScheduleIcon/>
+            <p>{daySelected.format('dddd, MMMM DD')}</p>
+          </div>
+          <div className='event-modal__element'>
+            <DescriptionIcon/>
+            <input 
+              type='text'
+              name='description'
+              placeholder='Add description'
+              className='event-modal__input'
+              required
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </div>
+          <div className='event-modal__element'> 
+            {labels.map((label, i) => (
+              <span
+                key={i}
+                onClick={() => setSelectedLabel(label)}
+                className='event-modal__label' 
+                style={{backgroundColor: label}}>
+                  {selectedLabel === label && <CheckIcon/>}
+                </span>
+            ))}
+          </div>
+          <button onClick={handleSubmit} type='submit' className='event-modal__save'>
+            {selectedEvent ? 'Update' : 'Save'}
+          </button>
+        </div>}
+        {selectedEvent && !isFormEdit &&
+        <div className='event-modal__elements'>
+          <div className='event-modal__element'>
+            <span className='event-modal__span' style={{backgroundColor: selectedLabel}}/>
+            <div>
+              <h2 className='event-modal__title'>
+                {title}
+              </h2>
+              <div className='event-modal__date'>
+                {dayjs(daySelected).format('dddd, MMMM D')}
+              </div>
+            </div>
+          </div>
+          <div className='event-modal__element'>
+            <DescriptionIcon/>
+            {description}
+          </div>
+        </div>}
       </form>
     </div>
   )

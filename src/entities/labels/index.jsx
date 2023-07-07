8b33977c6608cf2b53bfd253b348adc60b2f5cbe
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-07 11:57:10 +0300

    feat: add accordion to labels

--- src/entities/labels/index.jsx
+++ src/entities/labels/index.jsx
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import GlobalContext from '../../context/GlobalContext';
 import { Label } from '../../shared/ui/index';
 import './style.css';
 
 const Labels = () => {
   const {labels, updateLabel} = useContext(GlobalContext);
+  const [isActive, setIsActive] = useState(true);
   return (
     <div className="labels">
-    <p className="labels__title">Labels</p>
-    {labels.map(({ label: lbl, checked }, idx) => (
+    <div className="labels__title" onClick={() => setIsActive(!isActive)}>Labels</div>
+    {isActive &&
+    labels.map(({ label: lbl, checked }, idx) => (
       <Label lbl={lbl} checked={checked} idx={idx} updateLabel={() => updateLabel({ label: lbl, checked: !checked })} />
     ))}
     </div>

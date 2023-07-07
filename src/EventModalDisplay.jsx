41cfc998677f2e906fb4a2d6a60cc36464cba990
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-07 10:26:36 +0300

    feat: update styles for displayEvent

--- src/EventModalDisplay.jsx
+++ src/EventModalDisplay.jsx
@@ -5,6 +5,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import SubjectIcon from '@mui/icons-material/Subject';
+import EventIcon from '@mui/icons-material/Event';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import dayjs from 'dayjs';
 
 const labels = ['red', 'green', 'blue',  'purple', 'pink', 'orange'];
@@ -47,7 +49,9 @@ const EventModalDisplay = () => {
         {selectedEvent &&
         <div className='event-display__elements'>
           <div className='event-display__element'>
-            <span className='event-display__span' style={{backgroundColor: selectedLabel, marginTop: '0.375rem'}}/>
+            <div style={{alignSelf: 'flex-start', width: '1.25rem'}}>
+              <span className='event-display__span' style={{backgroundColor: selectedLabel, marginTop: '0.375rem'}}/>
+            </div>
             <div>
               <h2 className='event-display__title'>
                 {selectedEvent.title}
@@ -57,9 +61,18 @@ const EventModalDisplay = () => {
               </div>
             </div>
           </div>
-          <div className='event-modal__element'>
-            <SubjectIcon/>
+          {selectedEvent.description && 
+          <div className='event-display__element'>
+            <SubjectIcon className='event-display__element-left'/>
             {selectedEvent.description}
+          </div>}
+          <div className='event-display__element'>
+            <EventIcon className='event-display__element-left'/>
+            Eugene Zanevskij
+          </div>
+          <div className='event-display__element'>
+            <WorkOutlineIcon className='event-display__element-left'/>
+            Busy
           </div>
         </div>}
       </div>

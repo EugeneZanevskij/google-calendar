1351c8291dd4a1127e3a7895c94b13266cf2fc73
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-11 14:31:50 +0300

    feat: create useDay file

---
+++ src/entities/day/lib/useDay.js
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from 'react'
+import GlobalContext from '../../../context/GlobalContext';
+import dayjs from 'dayjs';
+
+export const useDay = (day) => {
+  const {daySelected, setDaySelected, setShowEventModal, filteredEvents} = useContext(GlobalContext);
+  const [dayEvents, setDayEvents] = useState([]);
+  useEffect(() => {
+    const events = filteredEvents.filter((event) => {
+      return dayjs(event.day).format('YYYY-MM-DD') === dayjs(day).format('YYYY-MM-DD');
+    });
+    setDayEvents(events);
+  }, [filteredEvents, day]);
+
+  function handleDayClick() {
+    setDaySelected(day);
+    setShowEventModal(true);
+  }
+  function getDayClass() {
+    const format = 'DD/MM/YYYY';
+    const today = dayjs().format(format);
+    const currentDay = day.format(format);
+    const selDay = daySelected && daySelected.format(format);
+    if (today === currentDay) {
+      return 'day--current';
+    } else if (selDay === currentDay) {
+      return 'day--selected';
+    } else {
+      return '';
+    }
+  }
+  
+  const getMonthName = (day.date() === 1) ? day.format('MMM') : '';
+  return ({
+    dayEvents,
+    handleDayClick,
+    getDayClass,
+    getMonthName
+  })
+}
--- src/entities/day/ui/Day.jsx
+++ src/entities/day/ui/Day.jsx
@@ -1,44 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import dayjs from 'dayjs';
 import './Day.css';
+import { useDay } from '../lib/useDay';
 import { DayEvent } from '../../dayEvent';
-import GlobalContext from '../../../context/GlobalContext';
 
 export const Day = ({day, index}) => {
-  const {daySelected, setDaySelected, setShowEventModal, filteredEvents} = useContext(GlobalContext);
-  const [dayEvents, setDayEvents] = useState([]);
-  useEffect(() => {
-    const events = filteredEvents.filter((event) => {
-      return dayjs(event.day).format('YYYY-MM-DD') === dayjs(day).format('YYYY-MM-DD');
-    });
-    setDayEvents(events);
-  }, [filteredEvents, day]);
-  function getDayClass(day) {
-    const format = 'DD/MM/YYYY';
-    const today = dayjs().format(format);
-    const currentDay = day.format(format);
-    const selDay = daySelected && daySelected.format(format);
-    if (today === currentDay) {
-      return 'day--current';
-    } else if (selDay === currentDay) {
-      return 'day--selected';
-    } else {
-      return '';
-    }
-  }
-
-  function getMonthName(day) {
-    if (day.date() === 1) return day.format('MMM');
-    return '';
-  }
+  const {dayEvents, handleDayClick, getDayClass, getMonthName} = useDay(day);
 
   return (
     <>
       {day && <div 
-        onClick={() => {
-          setDaySelected(day);
-          setShowEventModal(true);
-        }}
+        onClick={handleDayClick}
         className='day'
       >
         <div className='day__container'>
@@ -46,8 +18,8 @@ export const Day = ({day, index}) => {
             <p className='day__weekday'>
             {day.format('dd')}
           </p>}
-          <p className={`day__date ${getDayClass(day)} ${ day < dayjs() ? 'day__date--prev' : ''}`}>
-            {`${day.format('D')} ${getMonthName(day)}`}
+          <p className={`day__date ${getDayClass()} ${ day < dayjs() ? 'day__date--prev' : ''}`}>
+            {`${day.format('D')} ${getMonthName}`}
           </p>
         </div>
         {dayEvents.map((event, i) => {

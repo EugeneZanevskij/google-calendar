aa68cf08501733e852ca28f18096766017d9c3a4
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-11 16:52:13 +0300

    feat: create useEventForm model

--- src/features/eventModalForm/ui/index.jsx
+++ src/features/eventModalForm/ui/index.jsx
@@ -6,49 +6,18 @@ import CheckIcon from '@mui/icons-material/Check';
 import SubjectIcon from '@mui/icons-material/Subject';
 import "./EventModalForm.css";
 import dayjs from 'dayjs';
+import { useEventForm } from '../model';
 
 const labels = ['red', 'green', 'blue',  'purple', 'pink', 'orange'];
 
 export const EventModalForm = () => {
-  const {setShowEventModal, daySelected, setDaySelected, dispatchCalEvents, selectedEvent} = useContext(GlobalContext);
-  const [title, setTitle] = useState(
-    selectedEvent ? selectedEvent.title : ''
-  );
-  const [description, setDescription] = useState(
-    selectedEvent ? selectedEvent.description : ''
-  );
-  const [selectedLabel, setSelectedLabel] = useState(
-    selectedEvent ? labels.find(label => label === selectedEvent.label) : labels[0]
-  );
-  function handleSubmit (e) {
-    e.preventDefault();
-    const calEvent = {
-      title,
-      description,
-      label: selectedLabel,
-      day: daySelected,
-      id: selectedEvent ? selectedEvent.id : Date.now()
-    };
-    calEvent.title = calEvent.title ? calEvent.title : '(No title)';
-    if (selectedEvent) {
-      dispatchCalEvents({
-        type: 'update',
-        payload: calEvent
-      });
-    } else {
-    dispatchCalEvents({
-      type: 'push',
-      payload: calEvent
-    })
-    };
-    setShowEventModal(false);
-  };
+  const {title, description, selectedLabel, daySelected, setTitle, setDescription, setSelectedLabel, setDaySelected, closeEventForm, handleSubmit } = useEventForm();
 
   return (
     <div className='event-modal'>
       <form className='event-modal__form' onSubmit={handleSubmit}>
         <div className='event-modal__header' >
-          <button onClick={() => setShowEventModal(false)} className='event-modal__close'>
+          <button onClick={closeEventForm} className='event-modal__close'>
             <CloseIcon/>
           </button>
         </div>
@@ -102,7 +71,7 @@ export const EventModalForm = () => {
           ))}
         </div>
         <button onClick={handleSubmit} type='submit' className='event-modal__save'>
-          {selectedEvent ? 'Update' : 'Save'}
+          'Save'
         </button>
       </div>
       </form>

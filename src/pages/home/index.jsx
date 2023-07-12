173074c86bfc1a867f5fb91f0c99eafb29ab0e42
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-12 11:59:02 +0300

    refactor: apply prettier to pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,26 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { getMonth } from '../../util';
-import {EventModalDisplay} from '../../entities/eventModalDisplay';
-import {EventModalForm} from '../../features/eventModalForm';
-import {Header} from '../../widgets/header';
-import {Sidebar} from '../../widgets/sidebar';
-import {Calendar} from '../../widgets/calendar';
-import GlobalContext from '../../context/GlobalContext';
+import React, { useContext, useEffect, useState } from "react";
+import { getMonth } from "../../util";
+import { EventModalDisplay } from "../../entities/eventModalDisplay";
+import { EventModalForm } from "../../features/eventModalForm";
+import { Header } from "../../widgets/header";
+import { Sidebar } from "../../widgets/sidebar";
+import { Calendar } from "../../widgets/calendar";
+import GlobalContext from "../../context/GlobalContext";
 
 export const Home = () => {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
-  const {monthIndex, showEventModal, displayEvent} = useContext(GlobalContext);
+  const { monthIndex, showEventModal, displayEvent } =
+    useContext(GlobalContext);
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
   }, [monthIndex]);
   return (
     <>
-      {showEventModal && <EventModalForm/>}
-      {displayEvent && <EventModalDisplay/>}
-      <Header/>
+      {showEventModal && <EventModalForm />}
+      {displayEvent && <EventModalDisplay />}
+      <Header />
       <main className="main">
-        <Sidebar/>
-        <Calendar month={currentMonth}/>
+        <Sidebar />
+        <Calendar month={currentMonth} />
       </main>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./home";
 
 export const Routing = () => {
   return (

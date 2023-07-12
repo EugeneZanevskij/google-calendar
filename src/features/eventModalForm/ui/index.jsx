1569a6ce26f6264dfa44bb9234eae622045cd04e
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-07-12 11:36:11 +0300

    refactor: apply prettier to EventModalForm

--- src/features/eventModalForm/ui/index.jsx
+++ src/features/eventModalForm/ui/index.jsx
@@ -1,49 +1,60 @@
-import React from 'react';
-import CloseIcon from '@mui/icons-material/Close';
-import ScheduleIcon from '@mui/icons-material/Schedule';
-import CheckIcon from '@mui/icons-material/Check';
-import SubjectIcon from '@mui/icons-material/Subject';
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import SubjectIcon from "@mui/icons-material/Subject";
 import "./EventModalForm.css";
-import dayjs from 'dayjs';
-import { useEventForm } from '../model/useEventForm';
-import { Input, LabelInputForm } from '../../../shared/ui';
-import { EventModalElement } from '../../../entities/eventModalElement';
-import { LabelsInputs } from '../../../entities/LabelsInputs';
+import dayjs from "dayjs";
+import { useEventForm } from "../model/useEventForm";
+import { Input } from "../../../shared/ui";
+import { EventModalElement } from "../../../entities/eventModalElement";
+import { LabelsInputs } from "../../../entities/LabelsInputs";
 
 export const EventModalForm = () => {
-  const {labels, title, description, selectedLabel, daySelected, setTitle, setDescription, setSelectedLabel, setDaySelected, closeEventForm, handleSubmit } = useEventForm();
+  const {
+    labels,
+    title,
+    description,
+    selectedLabel,
+    daySelected,
+    setTitle,
+    setDescription,
+    setSelectedLabel,
+    setDaySelected,
+    closeEventForm,
+    handleSubmit,
+  } = useEventForm();
 
   return (
-    <div className='event-modal'>
-      <form className='event-modal__form' onSubmit={handleSubmit}>
-        <div className='event-modal__header' >
-          <button onClick={closeEventForm} className='event-modal__close'>
-            <CloseIcon/>
+    <div className="event-modal">
+      <form className="event-modal__form" onSubmit={handleSubmit}>
+        <div className="event-modal__header">
+          <button onClick={closeEventForm} className="event-modal__close">
+            <CloseIcon />
           </button>
         </div>
         <EventModalElement
           children={
             <Input
-              type='text'
-              name='title'
-              placeholder='Add title'
+              type="text"
+              name="title"
+              placeholder="Add title"
               value={title}
-              classStyle='event-modal__input event-modal__input--title'
-              onChange={e => setTitle(e.target.value)}
+              classStyle="event-modal__input event-modal__input--title"
+              onChange={(e) => setTitle(e.target.value)}
             />
           }
         />
-        <div className='event-modal__elements'>
+        <div className="event-modal__elements">
           <EventModalElement
             icon={<ScheduleIcon />}
             children={
               <Input
-                type='date'
-                name='date'
-                placeholder='Add date'
-                value={daySelected.format('YYYY-MM-DD')}
-                classStyle='event-modal__input-date'
-                onChange={e => setDaySelected(dayjs(e.target.value))}
+                type="date"
+                name="date"
+                placeholder="Add date"
+                value={daySelected.format("YYYY-MM-DD")}
+                classStyle="event-modal__input-date"
+                onChange={(e) => setDaySelected(dayjs(e.target.value))}
               />
             }
           />
@@ -51,21 +62,25 @@ export const EventModalForm = () => {
             icon={<SubjectIcon />}
             children={
               <textarea
-              name='description'
-              placeholder='Add description'
-              value={description}
-              className='event-modal__input event-modal__textarea'
-              onChange={e => setDescription(e.target.value)}
-            />
+                name="description"
+                placeholder="Add description"
+                value={description}
+                className="event-modal__input event-modal__textarea"
+                onChange={(e) => setDescription(e.target.value)}
+              />
             }
           />
-          <LabelsInputs 
-            labels={labels} 
-            selectedLabel={selectedLabel} 
+          <LabelsInputs
+            labels={labels}
+            selectedLabel={selectedLabel}
             setSelectedLabel={setSelectedLabel}
           />
-          <button onClick={handleSubmit} type='submit' className='event-modal__save'>
-            'Save'
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="event-modal__save"
+          >
+            Save
           </button>
         </div>
       </form>
